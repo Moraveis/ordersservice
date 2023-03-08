97c5e3957c4acb4d5138148df82d06f745a08401
Joao Vitor <joaovitormoraveis@gmail.com>
2023-03-07 21:14:11 -0500

    Implementing Order saga start handler

---
+++ src/main/java/com/joao/studycase/ordersservice/saga/OrderSaga.java
@@ -0,0 +1,43 @@
+package com.joao.studycase.ordersservice.saga;
+
+import com.joao.studycase.ordersservice.core.ReserveProductCommand;
+import com.joao.studycase.ordersservice.events.OrderCreatedEvent;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.spring.stereotype.Saga;
+
+import javax.annotation.Nonnull;
+
+@Saga
+public class OrderSaga {
+
+    private final CommandGateway commandGateway;
+
+    public OrderSaga(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent orderCreatedEvent) {
+        ReserveProductCommand reserveProductCommand = ReserveProductCommand.builder()
+                .orderId(orderCreatedEvent.getOrderId())
+                .productId(orderCreatedEvent.getProductId())
+                .userId(orderCreatedEvent.getUserId())
+                .quantity(orderCreatedEvent.getQuantity())
+                .build();
+
+        commandGateway.send(reserveProductCommand, new CommandCallback<ReserveProductCommand, Object>() {
+            @Override
+            public void onResult(@Nonnull CommandMessage<? extends ReserveProductCommand> commandMessage, @Nonnull CommandResultMessage<?> commandResultMessage) {
+                if (commandResultMessage.isExceptional()) {
+                    // TODO: implement compensation transaction
+                }
+            }
+        });
+    }
+}
